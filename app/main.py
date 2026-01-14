ea417fa2d957a7b4881c431ec1f10848ee1746e4
palivela13 <palivela13@gmail.com>
2026-01-14 14:38:49 +0530

    Week 1 day 2 assignment

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from app.agent import run_agent
 
 if __name__ == "__main__":
-    goal = "Explain what an AI agent is in one sentence"
+    goal = "Explain what LLM in simple word?"
     run_agent(goal)
