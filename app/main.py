f3a9d6e41014b492ec6c89fff23fdcb305b91daf
palivela13 <palivela13@gmail.com>
2026-01-19 13:28:38 +0530

    Week 1 Lecture 3: News Research Agent

--- app/main.py
+++ app/main.py
@@ -1,5 +1,13 @@
+# from app.agent import run_agent
+
+# if __name__ == "__main__":
+#     goal = "Explain what LLM in simple word?"
+#     run_agent(goal)
+
+
+
 from app.agent import run_agent
 
 if __name__ == "__main__":
-    goal = "Explain what LLM in simple word?"
+    goal = "Summarize current technology trends"
     run_agent(goal)
--- app/tools.py
+++ app/tools.py
@@ -1,4 +1,10 @@
+# def print_tool(text: str) -> str:
+#     print("[TOOL] Printing output:")
+#     print(text)
+#     return "printed"
+
+
 def print_tool(text: str) -> str:
-    print("[TOOL] Printing output:")
+    print("[TOOL] NEWS SUMMARY:")
     print(text)
     return "printed"
